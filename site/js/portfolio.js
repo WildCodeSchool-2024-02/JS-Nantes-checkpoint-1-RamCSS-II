0a617b1145c4ba53452aa2b9160d5ac975b7b13f
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-03-25 11:25:38 +0100

    JS step2 OK

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,15 +1,35 @@
 // Step 1
 
-const avatar=document.getElementById("avatar");
+const avatar = document.getElementById("avatar");
 
-avatar.addEventListener("click",()=>{  
-    // not very clean, like not clean at all, it's break at deployment.
-    if (avatar.src == "http://127.0.0.1:5500/site/image/avatar-bis.png"){
-    avatar.src = "./image/avatar.svg"
-} else {
-    console.log("movement")
-    avatar.src ="./image/avatar-bis.png";
-}});
+avatar.addEventListener("click", () => {
+    // not very clean, like not clean at all, it'll break at deployment.
+    if (avatar.src == "http://127.0.0.1:5500/site/image/avatar-bis.png") {
+        avatar.src = "./image/avatar.svg"
+    } else {
+        console.log("movement")
+        avatar.src = "./image/avatar-bis.png";
+    }
+});
 
 
 
+// Step 2 
+
+const nameButton = document.getElementById("name-editor-button");
+const firstname = document.getElementById("firstname");
+const desciptionSection = document.querySelector(".description");
+
+
+
+nameButton.addEventListener("click", () => {
+
+    // i put a color as the default here for testing purposes.
+    desciptionSection.style.backgroundColor =prompt("enter a #color","#750ff7");
+    desciptionSection.classList.remove("pink-bg");
+    firstname.innerText = prompt("what's your name, kid ?");
+    firstname.style.color = "white";
+
+
+
+})
